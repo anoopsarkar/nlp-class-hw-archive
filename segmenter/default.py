a0d010ff229eb51fa112dff9959651a56ecbb70f
Anoop Sarkar <anoop.sarkar@gmail.com>
2014-09-11 10:22:45 -0700

    added some more code to help reduce startup cost to interesting solutions

--- segmenter/default.py
+++ segmenter/default.py
@@ -2,9 +2,29 @@
 import sys, codecs, optparse, os
 
 optparser = optparse.OptionParser()
-optparser.add_option("-c", "--unigramcounts", dest='counts', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
+optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
+optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
 optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
 (opts, _) = optparser.parse_args()
+
+class Pdist(dict):
+    "A probability distribution estimated from counts in datafile."
+    def __init__(self, data=[]):
+        for key,count in data:
+            self[key] = self.get(key, 0) + int(count)
+        self.N = float(sum(self.itervalues()))
+        self.missingfn = (lambda k, N: 1./N)
+    def __call__(self, key): 
+        if key in self: return self[key]/self.N  
+        else: return self.missingfn(key, self.N)
+
+def datafile(name, sep='\t'):
+    "Read key,value pairs from file."
+    for line in file(name):
+        yield line.split(sep)
+
+# the default segmenter does not use any probabilities, but you could ...
+Pw  = Pdist(datafile(opts.counts1w))
 
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
