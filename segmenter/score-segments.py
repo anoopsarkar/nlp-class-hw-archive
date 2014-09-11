265772d615c4fd9d8df924ea139ab03b655e7017
Anoop Sarkar <anoop.sarkar@gmail.com>
2014-09-11 00:50:22 -0700

    uses stdin now

--- segmenter/score-segments.py
+++ segmenter/score-segments.py
@@ -3,12 +3,15 @@ import optparse, sys, codecs
 from collections import defaultdict
 
 optparser = optparse.OptionParser()
-optparser.add_option("-t", "--testfile", dest="testfile", default="output", help="Output from your segmenter program")
+optparser.add_option("-t", "--testfile", dest="testfile", default=None, help="Output from your segmenter program")
 optparser.add_option("-r", "--referencefile", dest="referencefile", default="data/reference", help="Reference segmentation")
 (opts, _) = optparser.parse_args()
 
-with open(opts.testfile) as f:
-    test = list(f)
+if opts.testfile is None:
+    test = list(sys.stdin)
+else:
+    with open(opts.testfile) as f:
+        test = list(f)
 with open(opts.referencefile) as f:
     reference = list(f)
 
