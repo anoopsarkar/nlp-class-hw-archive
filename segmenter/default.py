3eb2c02b987bcb088e02aa8e125fc817af33939f
Anoop Sarkar <anoop@sfu.ca>
2014-09-12 17:09:03 -0700

    better to have utf8 keys in Pdist

--- segmenter/default.py
+++ segmenter/default.py
@@ -9,22 +9,26 @@ optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('da
 
 class Pdist(dict):
     "A probability distribution estimated from counts in datafile."
-    def __init__(self, data=[]):
-        for key,count in data:
-            self[key] = self.get(key, 0) + int(count)
-        self.N = float(sum(self.itervalues()))
-        self.missingfn = (lambda k, N: 1./N)
-    def __call__(self, key): 
-        if key in self: return self[key]/self.N  
+
+    def __init__(self, filename, sep='\t', N=None, missingfn=None):
+        self.maxlen = 0 
+        for line in file(filename):
+            (key, freq) = line.split(sep)
+            try:
+                utf8key = unicode(key, 'utf-8')
+            except:
+                raise ValueError("Unexpected error %s" % (sys.exc_info()[0]))
+            self[utf8key] = self.get(utf8key, 0) + int(freq)
+            self.maxlen = max(len(utf8key), self.maxlen)
+        self.N = float(N or sum(self.itervalues()))
+        self.missingfn = missingfn or (lambda k, N: 1./N)
+
+    def __call__(self, key):
+        if key in self: return log10(float(self[key])/float(self.N))
         else: return self.missingfn(key, self.N)
 
-def datafile(name, sep='\t'):
-    "Read key,value pairs from file."
-    for line in file(name):
-        yield line.split(sep)
-
 # the default segmenter does not use any probabilities, but you could ...
-Pw  = Pdist(datafile(opts.counts1w))
+Pw  = Pdist(opts.counts1w)
 
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
