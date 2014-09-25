5cb766b739d2d36f412a067392a92ccd745cc9d4
Anoop Sarkar <anoop.sarkar@gmail.com>
2014-09-25 09:19:15 -0700

    added starter source code and data files

---
+++ chunker/default.py
@@ -0,0 +1,53 @@
+"""
+
+You have to write the perc_train function that trains the feature weights using the perceptron algorithm for the CoNLL 2000 chunking task.
+
+Each element of train_data is a (labeled_list, feat_list) pair. 
+
+Inside the perceptron training loop:
+
+    - Call perc_test to get the tagging based on the current feat_vec and compare it with the true output from the labeled_list
+
+    - If the output is incorrect then we have to update feat_vec (the weight vector)
+
+    - In the notation used in the paper we have w = w_0, w_1, ..., w_n corresponding to \phi_0(x,y), \phi_1(x,y), ..., \phi_n(x,y)
+
+    - Instead of indexing each feature with an integer we index each feature using a string we called feature_id
+
+    - The feature_id is constructed using the elements of feat_list (which correspond to x above) combined with the output tag (which correspond to y above)
+
+    - The function perc_test shows how the feature_id is constructed for each word in the input, including the bigram feature "B:" which is a special case
+
+    - feat_vec[feature_id] is the weight associated with feature_id
+
+    - This dictionary lookup lets us implement a sparse vector dot product where any feature_id not used in a particular example does not participate in the dot product
+
+    - To save space and time make sure you do not store zero values in the feat_vec dictionary which can happen if \phi(x_i,y_i) - \phi(x_i,y_{perc_test}) results in a zero value
+
+    - If you are going word by word to check if the predicted tag is equal to the true tag, there is a corner case where the bigram 'T_{i-1} T_i' is incorrect even though T_i is correct.
+
+"""
+
+import perc
+
+def perc_train(train_data, tagset):
+    feat_vec = {}
+    # insert your code here
+    return feat_vec
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 5:
+        print >>sys.stderr, "usage: %s tagset trainfile featfile modelfile" % sys.argv[0]
+        sys.exit(-1)
+    # each element in the feat_vec dictionary is:
+    # key=feature_id value=weight
+    feat_vec = {}
+    tagset = []
+    train_data = []
+
+    tagset = perc.read_tagset(sys.argv[1])
+    train_data = perc.read_labeled_data(sys.argv[2], sys.argv[3])
+    feat_vec = perc_train(train_data, tagset)
+    perc.perc_write_to_file(feat_vec, sys.argv[4])
+
