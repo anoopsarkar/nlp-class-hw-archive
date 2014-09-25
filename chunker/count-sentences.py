b992f5fd1b1e04b09d0083d64f33078b81a53425
Anoop Sarkar <anoop.sarkar@gmail.com>
2014-09-25 10:26:23 -0700

    revert to optparse; I dislike argparse

--- chunker/count-sentences.py
+++ chunker/count-sentences.py
@@ -7,9 +7,7 @@
 # python count.py -i data/input.feats.gz
 # python count.py -i data/input.txt.gz
 
-"""Count the number of sentences in a file that has one word per line and two newlines to separate sentences"""
-
-import sys, argparse, re, gzip, logging
+import sys, optparse, re, gzip, logging
 
 def countSentences(handle):
     contents = re.sub(r'\n\s*\n', r'\n\n', handle.read())
@@ -17,17 +15,16 @@ def countSentences(handle):
     return len(contents.split('\n\n'))
 
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser(description=__doc__)
-#    optparser = optparse.OptionParser()
-#    optparser.add_option("-i", "--inputfile", dest="inputfile", default=None, help="filename in the conll format")
-#    (opts, _) = optparser.parse_args()
-#    if opts.inputfile is None:
-#        logging.warning("using standard input")
-#        print countSentences(sys.stdin)
-#    elif opts.inputfile[-3:] == '.gz':
-#        with gzip.open(opts.inputfile) as f:
-#            print countSentences(f)
-#    else:
-#        with open(opts.inputfile) as f:
-#            print countSentences(f)
+    optparser = optparse.OptionParser()
+    optparser.add_option("-i", "--inputfile", dest="inputfile", default=None, help="filename in the conll format")
+    (opts, _) = optparser.parse_args()
+    if opts.inputfile is None:
+        logging.warning("using standard input")
+        print countSentences(sys.stdin)
+    elif opts.inputfile[-3:] == '.gz':
+        with gzip.open(opts.inputfile) as f:
+            print countSentences(f)
+    else:
+        with open(opts.inputfile) as f:
+            print countSentences(f)
 
