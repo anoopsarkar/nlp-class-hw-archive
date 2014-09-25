98c28d18edbefeecb9b559b5b83a5abb67e40aff
Anoop Sarkar <anoop.sarkar@gmail.com>
2014-09-25 09:09:27 -0700

    users might provide filename in argv so warn them to use -i

--- chunker/count-sentences.py
+++ chunker/count-sentences.py
@@ -7,7 +7,7 @@
 # python count.py -i data/input.feats.gz
 # python count.py -i data/input.txt.gz
 
-import sys, optparse, re, gzip
+import sys, optparse, re, gzip, logging
 
 def countSentences(handle):
     contents = re.sub(r'\n\s*\n', r'\n\n', handle.read())
@@ -19,6 +19,7 @@ if __name__ == '__main__':
     optparser.add_option("-i", "--inputfile", dest="inputfile", default=None, help="filename in the conll format")
     (opts, _) = optparser.parse_args()
     if opts.inputfile is None:
+        logging.warning("using standard input")
         print countSentences(sys.stdin)
     elif opts.inputfile[-3:] == '.gz':
         with gzip.open(opts.inputfile) as f:
